132bf8a26acea079d5312b355a532dac78a702b4
cmgvesth <tammivesth@hotmail.com>
2015-02-25 16:44:05 +0100

    tcve feb 25

--- tcve/blast_antismash_subset.R
+++ tcve/blast_antismash_subset.R
@@ -30,20 +30,80 @@ subdat <- subset(tmpdat, tmpdat$clust_size>10 & tmpdat$sec1=="Nigri" & tmpdat$se
 subdat <- subset(tmpdat, tmpdat$clust_size>10 & tmpdat$sec1=="Nigri" & tmpdat$sec2=="Nigri" & tmpdat$org_id=="10" |  tmpdat$org_id=="2", select=c(1,2,4,7))
 # mdat <- melt(subdat, id=c("clust_id","h_org","org_id"))
 # dat <- cast(mdat, h_org+clust_id~clustCov, value="clustCov", fill=0)
-subdat <- subset(tmpdat, tmpdat$clust_size>3 & tmpdat$sec1=="Nigri" & tmpdat$sec2=="Nigri", select=c(1,2,4,7))
+subdat <- subset(tmpdat, tmpdat$sec1=="Nigri" & tmpdat$sec2=="Nigri", select=c(1,2,4,7))
 
 dat <- dcast(subdat, org_id+clust_id~h_org, value.var="clustCov", fill=0) # WOOOOOOOOOO 
 mat <- t(data.matrix(dat[,3:ncol(dat)])) # NOTE transpose
 colnames(mat) <- dat$clust_id
 
 cvs <- unique(dat$org_id)
+#cvs_color <- sample(rainbow(length(cvs)), length(cvs_color))
 cvs_color <- rainbow(length(cvs))
 ccol_stages <- sapply(dat$org_id, function(x) cvs_color[x==cvs])
 
 b <- c(0,0.2,0.4,0.6,0.8,1)
+
+pdf('orgs2cluster-all-cutoff-clustered.pdf')
+colfunc <- c("white"  ,"white", "white" ,"#2B8CBE","#084081")
+heatmap.2(mat, main="All nigri clusters, mostly white, clustered", dendrogram="both", trace = "none", ColSideColor=ccol_stages, Rowv=TRUE, Colv=TRUE ,col=colfunc, breaks=b, labCol=FALSE, density.info="none", margins =c(4,10))
+dev.off()
+
+pdf('orgs2cluster-all-range-clustered.pdf')
 colfunc <- c("white"  ,"#CCEBC5", "#7BCCC4" ,"#2B8CBE","#084081")
-#colfunc <- c("white"  ,"white", "white" ,"#2B8CBE","#084081")
-heatmap.2(mat, dendrogram="none", trace = "none", ColSideColor=ccol_stages,Rowv=TRUE, Colv=TRUE , col=colfunc, breaks=b, labCol=FALSE)
+heatmap.2(mat, main="All nigri clusters, clustered",dendrogram="both", trace = "none", ColSideColor=ccol_stages, Rowv=TRUE, Colv=TRUE , col=colfunc, breaks=b, labCol=FALSE, density.info="none", margins =c(4,10))
+dev.off()
+
+pdf('orgs2cluster-all-cutoff-unclustered.pdf')
+colfunc <- c("white"  ,"white", "white" ,"#2B8CBE","#084081")
+heatmap.2(mat, main="All nigri clusters, unclustered", dendrogram="none", trace = "none", ColSideColor=ccol_stages, Rowv=FALSE, Colv=FALSE , col=colfunc, breaks=b, labCol=FALSE, density.info="none", margins =c(4,10))
+dev.off()
+
+pdf('orgs2cluster-all-cutoff-orgclustered.pdf')
+colfunc <- c("white"  ,"white", "white" ,"#2B8CBE","#084081")
+heatmap.2(mat, main="All nigri clusters, organism clustered", dendrogram="row", trace = "none", ColSideColor=ccol_stages, Rowv=TRUE, Colv=FALSE , col=colfunc, breaks=b, labCol=FALSE, density.info="none", margins =c(4,10))
+dev.off()
+
+
+
+
+
+
+
+
+
+
+subdat <- subset(tmpdat, tmpdat$sec1=="Nigri" & tmpdat$sec2=="Nigri" & tmpdat$org_id=="22", select=c(1,2,4,7))
+
+dat <- dcast(subdat, org_id+clust_id~h_org, value.var="clustCov", fill=0) # WOOOOOOOOOO 
+mat <- t(data.matrix(dat[,3:ncol(dat)])) # NOTE transpose
+colnames(mat) <- dat$clust_id
+
+cvs <- unique(dat$org_id)
+#cvs_color <- sample(rainbow(length(cvs)), length(cvs_color))
+cvs_color <- rainbow(length(cvs))
+ccol_stages <- sapply(dat$org_id, function(x) cvs_color[x==cvs])
+
+b <- c(0,0.2,0.4,0.6,0.8,1)
+
+pdf('orgs2cluster-Aspni7-cutoff-clustered.pdf')
+colfunc <- c("white"  ,"white", "white" ,"#2B8CBE","#084081")
+heatmap.2(mat, main="Aspni7 nigri clusters, mostly white, clustered", dendrogram="both", trace = "none", ColSideColor=ccol_stages, Rowv=TRUE, Colv=TRUE ,col=colfunc, breaks=b, labCol=FALSE, density.info="none", margins =c(4,10))
+dev.off()
+
+pdf('orgs2cluster-Aspni7-range-clustered.pdf')
+colfunc <- c("white"  ,"#CCEBC5", "#7BCCC4" ,"#2B8CBE","#084081")
+heatmap.2(mat, main="Aspni7 nigri clusters, clustered",dendrogram="both", trace = "none", ColSideColor=ccol_stages, Rowv=TRUE, Colv=TRUE , col=colfunc, breaks=b, labCol=FALSE, density.info="none", margins =c(4,10))
+dev.off()
+
+pdf('orgs2cluster-Aspni7-cutoff-unclustered.pdf')
+colfunc <- c("white"  ,"white", "white" ,"#2B8CBE","#084081")
+heatmap.2(mat, main="Aspni7 nigri clusters, unclustered", dendrogram="none", trace = "none", ColSideColor=ccol_stages, Rowv=FALSE, Colv=FALSE , col=colfunc, breaks=b, labCol=FALSE, density.info="none", margins =c(4,10))
+dev.off()
+
+pdf('orgs2cluster-Aspni7-cutoff-orgclustered.pdf')
+colfunc <- c("white"  ,"white", "white" ,"#2B8CBE","#084081")
+heatmap.2(mat, main="Aspni7 nigri clusters, organism clustered", dendrogram="row", trace = "none", ColSideColor=ccol_stages, Rowv=TRUE, Colv=FALSE , col=colfunc, breaks=b, labCol=FALSE, density.info="none", margins =c(4,10))
+dev.off()
 
 
 
@@ -56,8 +116,44 @@ heatmap.2(mat, dendrogram="none", trace = "none", ColSideColor=ccol_stages,Rowv=
 
 
 
+subdat <- subset(tmpdat, tmpdat$clust_size>10 & tmpdat$sec1=="Nigri" & tmpdat$sec2=="Nigri", select=c(1,2,4,7))
 
+dat <- dcast(subdat, org_id+clust_id~h_org, value.var="clustCov", fill=0) # WOOOOOOOOOO 
+mat <- t(data.matrix(dat[,3:ncol(dat)])) # NOTE transpose
+colnames(mat) <- dat$clust_id
 
+cvs <- unique(dat$org_id)
+#cvs_color <- sample(rainbow(length(cvs)), length(cvs_color))
+cvs_color <- rainbow(length(cvs))
+ccol_stages <- sapply(dat$org_id, function(x) cvs_color[x==cvs])
+
+b <- c(0,0.2,0.4,0.6,0.8,1)
+
+pdf('orgs2cluster-10-cutoff-clustered.pdf')
+colfunc <- c("white"  ,"white", "white" ,"#2B8CBE","#084081")
+heatmap.2(mat, main="Size >10 nigri clusters, mostly white, clustered", dendrogram="both", trace = "none", ColSideColor=ccol_stages, Rowv=TRUE, Colv=TRUE ,col=colfunc, breaks=b, labCol=FALSE, density.info="none", margins =c(4,10))
+dev.off()
+
+pdf('orgs2cluster-10-range-clustered.pdf')
+colfunc <- c("white"  ,"#CCEBC5", "#7BCCC4" ,"#2B8CBE","#084081")
+heatmap.2(mat, main="Size >10 nigri clusters, clustered",dendrogram="both", trace = "none", ColSideColor=ccol_stages, Rowv=TRUE, Colv=TRUE , col=colfunc, breaks=b, labCol=FALSE, density.info="none", margins =c(4,10))
+dev.off()
+
+pdf('orgs2cluster-10-cutoff-unclustered.pdf')
+colfunc <- c("white"  ,"white", "white" ,"#2B8CBE","#084081")
+heatmap.2(mat, main="Size >10 nigri clusters, unclustered", dendrogram="none", trace = "none", ColSideColor=ccol_stages, Rowv=FALSE, Colv=FALSE , col=colfunc, breaks=b, labCol=FALSE, density.info="none", margins =c(4,10))
+dev.off()
+
+pdf('orgs2cluster-10-cutoff-orgclustered.pdf')
+colfunc <- c("white"  ,"white", "white" ,"#2B8CBE","#084081")
+heatmap.2(mat, main="Size >10 nigri clusters, organism clustered", dendrogram="row", trace = "none", ColSideColor=ccol_stages, Rowv=TRUE, Colv=FALSE , col=colfunc, breaks=b, labCol=FALSE, density.info="none", margins =c(4,10))
+dev.off()
+
+pdf('orgs2cluster-color-pie.pdf')
+pie(rep(1,length(cvs_color)), col=cvs_color)
+dev.off()
+
+"""
 dat <- cast(subdat, h_org~clust_id+org_id, value="clustCov", fill=0)
 mat <- data.matrix(dat)
 rownames(mat) <- dat[,1]
@@ -74,3 +170,4 @@ mat <- data.matrix(dat)
 rownames(mat) <- dat[,1]
 mat <- mat[,-1:-2]
 heatmap.2(mat, dendrogram="both", trace = "none", col=colfunc)
+"""
